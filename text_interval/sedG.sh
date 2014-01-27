265017e2e5fecf6190ab204c88afd3ce8682f9a4
Chris Wong <wc38.wong@gmail.com>
2014-01-27 14:56:25 +0800

    Chris Wong is handsome

--- text_interval/sedG.sh
+++ text_interval/sedG.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/bash
 
-# Add a blank line after each line
-cat ti.txt | sed G
+if [ $# != 1 ]; then
+	echo "Usage: $0 [1~7]"
+	echo "1:Add a blank line after each line"	
+	echo "2:" 
+	exit 1
+fi
+
+case $1 in
+	"1")
+		# Add a blank line after each line
+		cat ti.txt | sed G
+		;;
+esac
