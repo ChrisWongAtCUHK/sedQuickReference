5a65b973119a06368b6861ea570e2cb0f9f3cc26
Chris Wong <wc38.wong@gmail.com>
2014-01-27 17:21:55 +0800

    Text interval

--- text_interval/sedG.sh
+++ text_interval/sedG.sh
@@ -2,8 +2,13 @@
 
 if [ $# != 1 ]; then
 	echo "Usage: $0 [1~7]"
-	echo "1:Add a blank line after each line"	
-	echo "2:" 
+	echo "1: Add a blank line after each line."	
+	echo "2: Delete all blank line, then add a blank line after each line." 
+	echo "3: Add two blank lines after each line."
+	echo "4: Delete all lines of even number."
+	echo "5: Add a blank line before pattern \"regex\"."
+	echo "6: Add a blank line after pattern \"regex\"."
+	echo "7: Add a blank line before and after pattern \"regex\"."
 	exit 1
 fi
 
@@ -11,5 +16,34 @@ case $1 in
 	"1")
 		# Add a blank line after each line
 		cat ti.txt | sed G
+		;;
+
+	"2")
+		# Delete all blank line, then add a blank line after each line."
+		cat ti.txt | sed '/^$/d;G'
+		;;
+	"3")
+		# Add two blank lines after each line.
+		cat ti.txt | sed 'G;G'
+		;;
+	
+	"4")
+		# Delete all lines of even number.
+		cat ti.txt | sed 'n;d'
+		;;
+
+	"5")
+        # Add a blank line before pattern "regex".
+		cat ti.txt | sed '/line5/{x;p;x;}'
+		;;
+
+	"6")
+        # Add a blank line after pattern "regex".
+		cat ti.txt | sed '/line5/G'
+		;;
+
+	"7")
+        # Add a blank line before and after pattern "regex".
+		cat ti.txt | sed '/line5/{x;p;x;G;}'
 		;;
 esac
--- text_interval/seddG.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-
-# Delete all blank line,
-#  then add a blank line after each line,
-#  such that each line will have only one blank line after it
-cat seddG.txt | sed '/^$/d;G'
