1456d68bef4642c216b0d6984030f28cde7716b7
Chris Wong <wc38.wong@gmail.com>
2014-01-28 14:25:19 +0800

    Text interval

--- text_interval/sedG.sh
+++ text_interval/sedG.sh
@@ -12,38 +12,40 @@ if [ $# != 1 ]; then
 	exit 1
 fi
 
+filename='lines.txt'
+
 case $1 in
 	"1")
 		# Add a blank line after each line
-		cat ti.txt | sed G
+		cat $filename | sed G
 		;;
 
 	"2")
 		# Delete all blank line, then add a blank line after each line."
-		cat ti.txt | sed '/^$/d;G'
+		cat $filename | sed '/^$/d;G'
 		;;
 	"3")
 		# Add two blank lines after each line.
-		cat ti.txt | sed 'G;G'
+		cat $filename | sed 'G;G'
 		;;
 	
 	"4")
 		# Delete all lines of even number.
-		cat ti.txt | sed 'n;d'
+		cat $filename | sed 'n;d'
 		;;
 
 	"5")
         # Add a blank line before pattern "regex".
-		cat ti.txt | sed '/line5/{x;p;x;}'
+		cat $filename | sed '/line5/{x;p;x;}'
 		;;
 
 	"6")
         # Add a blank line after pattern "regex".
-		cat ti.txt | sed '/line5/G'
+		cat $filename | sed '/line5/G'
 		;;
 
 	"7")
         # Add a blank line before and after pattern "regex".
-		cat ti.txt | sed '/line5/{x;p;x;G;}'
+		cat $filename | sed '/line5/{x;p;x;G;}'
 		;;
 esac
